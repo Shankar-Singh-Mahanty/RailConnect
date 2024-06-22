fd8b629313d2bd4fcc67146cd8555f07a34994c9
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-06-22 12:21:16 +0530

    Table Constraints Updated

--- MySQL/CUGDetails.sql
+++ MySQL/CUGDetails.sql
@@ -1,6 +1,7 @@
 CREATE TABLE CUGDetails (
     cug_id INT PRIMARY KEY,
-    cug_number VARCHAR(50) UNIQUE,
+    cug_number VARCHAR(50) UNIQUE NOT NULL,
+    emp_number VARCHAR(50) UNIQUE NOT NULL,
     empname VARCHAR(100) NOT NULL,
     designation VARCHAR(100) NOT NULL,
     division VARCHAR(100) NOT NULL,
@@ -13,5 +14,7 @@ CREATE TABLE CUGDetails (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    CONSTRAINT CHK_allocation CHECK (allocation >= 0)  -- Allocation should be non-negative
+    CONSTRAINT CHK_allocation CHECK (allocation >= 0),  -- Allocation should be non-negative
+    CONSTRAINT CHK_cug_number_length CHECK (LENGTH(cug_number) = 11),  -- CUG number must be 11 digits
+    CONSTRAINT CHK_emp_number_length CHECK (LENGTH(emp_number) = 12)  -- Employee number must be 12 digits long
 );
