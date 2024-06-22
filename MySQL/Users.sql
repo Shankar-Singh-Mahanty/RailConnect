e201eab4bc555b4fe7b5b82b1d4206cc343a4528
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-06-22 09:41:30 +0530

    organised

---
+++ MySQL/Users.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    contact VARCHAR(10) NOT NULL CHECK (LENGTH(contact) = 10 AND contact REGEXP '^[0-9]{10}$'),
+    address VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    role ENUM('admin', 'dealer') NOT NULL
+);
+
+Desc Users;
+
+INSERT INTO Users (username, email, password, contact, address, created_at, role) VALUES 
+('admin1', 'admin1@gmail.com', SHA2('pass001', 256), '8260529733', 'Bhubaneswar', CURRENT_TIMESTAMP, 'admin'),
+('admin2', 'admin2@gmail.com', SHA2('pass002', 256), '9437562924', 'Ghatika', CURRENT_TIMESTAMP, 'admin'),
+
+('dealer1', 'dealer1@gmail.com', SHA2('pass01', 256), '6371929991', 'Kenjhor', CURRENT_TIMESTAMP, 'dealer'),
+('dealer2', 'dealer2@gmail.com', SHA2('pass02', 256), '8895821654', 'Cuttack', CURRENT_TIMESTAMP, 'dealer');
+
+
+SELECT * FROM Users;
